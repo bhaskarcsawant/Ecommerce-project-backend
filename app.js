2f41ec9525201789220c942e4f3852628e7f31ba
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-03-07 19:39:33 +0530

    added userModel, userRegisterfunction, getAllusersfunction, createdJWTtoken for user, hashed password while saving

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ app.use(express.json())
 
 //Routes
 const product = require("./routes/ProductRoute")
+const user = require("./routes/UserRoute")
 
 app.use("/api/v1", product)
+app.use("/api/v1", user)
 
 module.exports = app
