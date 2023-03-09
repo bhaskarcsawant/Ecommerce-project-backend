eb9688c1294b8ee03fe0f885e25b3b16b7c9eba2
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-03-09 22:38:11 +0530

    provided role property while registering user, fixed user route issue, added isUserAdmin function to verify the user role if it's admin, passed the isUserAdmin function to the routes to verify is user is admin or not.

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -8,8 +8,6 @@ router.route('/users').get(isUserAdmin, getAllUsers)
 router.route('/register/').post(registerUser)
 router.route('/login/').post(loginUser)
 router.route('/logout/').get(isUserAuthenticated, logoutUser)
-// router.route('/product/new').post(addProduct)
-// router.route('/product/:id').patch(updateProduct).delete(deleteProduct).get(getSingleProduct)
-// router.route('/deleteproduct/:id').post(deleteProduct)
+
 
 module.exports = router
