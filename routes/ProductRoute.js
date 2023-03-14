917a945d2fe2d5136c1597dfc1ff4f786c7a38d7
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-03-14 21:04:58 +0530

    changed create update and delete routes to admin routes

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -7,8 +7,9 @@ const router = express.Router();
 
 
 router.route('/products').get(gelAllProducts)
-router.route('/product/new').post(isUserAdmin, addProduct)
-router.route('/product/:id').patch(isUserAdmin, updateProduct).delete(isUserAdmin, deleteProduct).get(getSingleProduct)
+router.route('/admin/product/new').post(isUserAdmin, addProduct)
+router.route('/admin/product/:id').patch(isUserAdmin, updateProduct).delete(isUserAdmin, deleteProduct)
+router.route('/product/:id').get(getSingleProduct)
 // router.route('/deleteproduct/:id').post(deleteProduct)
 
 module.exports = router
