4668677f05fb4cadf9ff693a70cd5125f5c59769
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-04-21 19:47:09 +0530

    corrected image upload

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const fileupload = require('express-fileupload');
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
-app.use(express.json())
+// app.use(express.json())
 app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileupload())
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 //Routes
 const product = require("./routes/ProductRoute")
